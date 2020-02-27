f299f4363c2d344c9f702781fb79138928221637
George G Tony <george.g@bridgeglobal.local>
2020-02-27 09:45:43 +0530

    Generate build using Bitrise

---
+++ app/src/main/java/com/example/trygithub_bitrisebuild/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.trygithub_bitrisebuild;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
